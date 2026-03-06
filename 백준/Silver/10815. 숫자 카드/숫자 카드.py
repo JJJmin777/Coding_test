7c80d52b97a899696d99bf2b1ecabf9bbbf9f4a5
JJJmin777 <dongmin753@naver.com>
2026-03-06 12:19:33 +0900

    [Silver V] Title: 숫자 카드, Time: 512 ms, Memory: 123776 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,23 +1,14 @@
 import sys
 
-to_list = [0 for i in range(20000000)] 
-ans_list = []
+result = []
 
 card_num = int(sys.stdin.readline().strip())
 
 card_list = list(map(int, sys.stdin.readline().strip().split()))
-
-for i in card_list:
-    to_list[i] = 1
+card_set = set(card_list)
 
 n = int(sys.stdin.readline().strip())
 
 s = list(map(int, sys.stdin.readline().strip().split()))
 
-for i in s:
-    if to_list[i] == 1:
-        ans_list.append(1)
-    else:
-        ans_list.append(0)
-
-print(*ans_list)
+print(*(1 if x in card_set else 0 for x in s))
