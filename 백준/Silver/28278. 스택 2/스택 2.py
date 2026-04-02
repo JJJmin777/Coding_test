6233505cb498c689525deb7d98fb767ade0db6b4
JJJmin777 <dongmin753@naver.com>
2026-04-02 14:07:09 +0900

    [Silver IV] Title: 스택 2, Time: 928 ms, Memory: 92508 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -2,6 +2,7 @@ import sys
 
 n = int(sys.stdin.readline().strip())
 stack = []
+result = []
 
 for _ in range(n):
     line = sys.stdin.readline().strip().split()
@@ -14,22 +15,27 @@ for _ in range(n):
         b = int(line[0])
 
         if b == 2:
-            if stack:
-                print(stack.pop())
-            else:
-                print(-1)
+            # if stack:
+            #     print(stack.pop())
+            # else:
+            #     print(-1)
+            result.append(str(stack.pop() if stack else -1))
 
         elif b == 3:
-            print(len(stack))
+            result.append(str(len(stack)))
 
         elif b == 4:
-            if stack:
-                print(0)
-            else:
-                print(1)
+            # if stack:
+            #     print(0)
+            # else:
+            #     print(1)
+            result.append('0' if stack else '1')
 
         elif b == 5:
-            if stack:
-                print(stack[-1])
-            else:
-                print(-1)
+            # if stack:
+            #     print(stack[-1])
+            # else:
+            #     print(-1)
+            result.append(str(stack[-1] if stack else -1))
+
+print('\n'.join(result))
